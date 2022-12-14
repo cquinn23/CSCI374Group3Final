496a4ff568444063cc7f72f511881fe454134fd9
cquinn23 <61425570+cquinn23@users.noreply.github.com>
2022-12-14 16:35:55 -0500

    Update nn.py

--- nn.py
+++ nn.py
@@ -7,21 +7,24 @@ from sklearn.neural_network import MLPRegressor
 #ok so MLPRegressor is basically just a classifier without an activation function so it gives us n variables as an output, i think we can train these to the stats we want to predict
 #also this has a partial_fit() function so if we wanted to go over the y2d results as a separate thing we could do that, the idea being past years have pretty representative power rankings, but in progress years might not be quite perfect
 
+def main(data, training, seed, games_to_predict):
+    model = train_test(data, training, seed)
+    preads = model.predict(games_to_predict)
 
-def test(data, training, seed):
+
+def train_test(data, training, seed):
     X, y = read_data(data)
     X_train, X_test, y_train, y_test = sklearn.train_test_split(X, y, random_state=1)
     model = MLPRegressor(random_state=seed, max_iter = 500).fit(X_train, y_train)
     a = model.predict(X_test)
     print(a)
     model.score(X_test, y_test)
+    return model
     
     
 
-def match_pred(data, T1, T2, seed):
-        X, y = read_data(data)
-        model = MLPRegressor(random_state=seed, max_iter = 500).fit(X, y)
-        x = get_game_stats(T1, T2)
+def match_pred(model, T1, T2):
+        x = get_team_stats(T1, T2)
         a = model.predict(x)
         return a
 
